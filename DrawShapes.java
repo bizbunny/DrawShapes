25690f88b0b55da73ff9d35a3fd38e5a5dfe26a0
bizbunny <41245033+bizbunny@users.noreply.github.com>
2020-01-22 09:40:39 -0600

    Add files via upload

---
+++ DrawShapes.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+import algs4.StdDraw;
+/*Anh Nguyen*/
+public class DrawShapes {
+
+	public static void main(String[] args) {
+		StdDraw.setPenColor(StdDraw.GREEN);
+		StdDraw.filledSquare(.25, .25, .25);
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(.25, .75, .25);
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledSquare(.75, .75, .25);
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(.75, .25, .25);
+	}
+	
+}
